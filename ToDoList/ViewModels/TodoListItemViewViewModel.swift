008cdbe5fb1942b9a97d9ec18968ba52bb829084
burak <burakaydin9761@gmail.com>
2023-07-01 15:02:27 +0300

    done Checkmarks

--- ToDoList/ViewModels/TodoListItemViewViewModel.swift
+++ ToDoList/ViewModels/TodoListItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Burak on 26.06.2023.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class TodoListItemViewViewModel: ObservableObject {
@@ -13,7 +15,19 @@ class TodoListItemViewViewModel: ObservableObject {
     init(){}
     
     func toggleIsDone(item: TodoListItem){
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
     
 }
